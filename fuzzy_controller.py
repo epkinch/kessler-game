c256439efd3a7efe8388a51de954c8017c6cebce
Spencer Schmidt <skschmid@ualberta.ca>
2025-12-01 16:01:13 -0700

    Modified fuzzy sets and added Vec2D class

--- fuzzy_controller.py
+++ fuzzy_controller.py
@@ -9,59 +9,70 @@ class FuzzyController(KesslerController):
     def __init__(self):
         self.eval_frames = 0
 
-        # Input variables
+        # Bullet time is the amount of time the bullet needs to reach its
+        # target.
         bullet_time = ctrl.Antecedent(np.arange(0, 1.0, 0.002), 'bullet_time')
-        theta_delta = ctrl.Antecedent(np.arange(-math.pi/30, math.pi/30, 0.1), 'theta_delta')
+
+        # Theta delta is the amount the ship needs to turn to complete its
+        # next action which can vary from -pi to +pi.
+        theta_delta = ctrl.Antecedent(np.linspace(-math.pi, math.pi, 20), 'theta_delta')
 
         # The following are for threat assessment
-        asteroid_distance = ctrl.Antecedent(np.arange(0, 1000, 10), 'asteroid_distance')
-        asteroid_size = ctrl.Antecedent(np.arange(10, 100, 5), 'asteroid_size')
-        asteroid_velocity = ctrl.Antecedent(np.arange(0, 400, 10), 'asteroid_velocity')
+        # Asteroid max distance is dependent on the map size which is by
+        # default 1000x800 giving a max distance on the diagonal.
+        asteroid_distance = ctrl.Antecedent(np.arange(0, 1300, 10), 'asteroid_distance')
+
+        # Asteroids can only be a size from 1 to 4
+        asteroid_size = ctrl.Antecedent(np.arange(1, 5, 1), 'asteroid_size')
+
+        # Initial max velocity of an asteroid is based on its size which is
+        # 165.0 m/s for a size 1 asteroid, see asteroid.py.
+        asteroid_velocity = ctrl.Antecedent(np.arange(0, 200, 10), 'asteroid_velocity')
 
         # Output variables
-        ship_turn = ctrl.Consequent(np.arange(-180, 180, 1), 'ship_turn')
-        ship_thrust = ctrl.Consequent(np.arange(-500, 500, 100), 'ship_thrust')
+        ship_turn = ctrl.Consequent(np.arange(-180.0, 180.0, 1.0), 'ship_turn')
+        ship_thrust = ctrl.Consequent(np.arange(-480.0, 480.0, 10.0), 'ship_thrust')
         ship_fire = ctrl.Consequent(np.arange(-1, 1, 0.1), 'ship_fire')
         ship_mine = ctrl.Consequent(np.arange(-1, 1, 0.1), 'ship_mine')
 
         # Bullet time sets
-        bullet_time['VS'] = fuzz.trimf(bullet_time.universe, [0, 0, 0.02]) # Very short
-        bullet_time['S'] = fuzz.trimf(bullet_time.universe, [0, 0.02, 0.05]) # Short
-        bullet_time['M'] = fuzz.trimf(bullet_time.universe, [0.02, 0.05, 0.1]) # Medium
-        bullet_time['L'] = fuzz.smf(bullet_time.universe, 0.05, 0.1) # Long
+        bullet_time['VS'] = fuzz.trimf(bullet_time.universe, [0, 0, 0.02])
+        bullet_time['S'] = fuzz.trimf(bullet_time.universe, [0, 0.02, 0.05])
+        bullet_time['M'] = fuzz.trimf(bullet_time.universe, [0.02, 0.05, 0.1])
+        bullet_time['L'] = fuzz.smf(bullet_time.universe, 0.05, 0.1)
 
         # Theta delta sets
-        theta_delta['NL'] = fuzz.zmf(theta_delta.universe, -math.pi, -math.pi/3)
-        theta_delta['NM'] = fuzz.trimf(theta_delta.universe, [-math.pi, -math.pi/3, -math.pi/6])
+        theta_delta['NL'] = fuzz.zmf(theta_delta.universe, -math.pi, -math.pi/2)
+        theta_delta['NM'] = fuzz.trimf(theta_delta.universe, [-math.pi, -math.pi/2, -math.pi/6])
         theta_delta['NS'] = fuzz.trimf(theta_delta.universe, [-math.pi/3, -math.pi/6, 0])
         theta_delta['Z'] = fuzz.trimf(theta_delta.universe, [-math.pi/6, 0, math.pi/6])
         theta_delta['PS'] = fuzz.trimf(theta_delta.universe, [0, math.pi/6, math.pi/3])
-        theta_delta['PM'] = fuzz.trimf(theta_delta.universe, [math.pi/6, math.pi/3, math.pi])
-        theta_delta['PL'] = fuzz.smf(theta_delta.universe, math.pi/3, math.pi)
+        theta_delta['PM'] = fuzz.trimf(theta_delta.universe, [math.pi/6, math.pi/2, math.pi])
+        theta_delta['PL'] = fuzz.smf(theta_delta.universe, math.pi/2, math.pi)
 
         # Asteroid distance sets
-        asteroid_distance['VClose'] = fuzz.trimf(asteroid_distance.universe, [0, 0, 150])
-        asteroid_distance['Close'] = fuzz.trimf(asteroid_distance.universe, [100, 200, 350])
-        asteroid_distance['Medium'] = fuzz.trimf(asteroid_distance.universe, [250, 400, 600])
-        asteroid_distance['Far'] = fuzz.trimf(asteroid_distance.universe, [450, 650, 900])
-        asteroid_distance['VFar'] = fuzz.smf(asteroid_distance.universe, 700, 900)
+        asteroid_distance['VClose'] = fuzz.zmf(asteroid_distance.universe, 0, 300)
+        asteroid_distance['Close'] = fuzz.trimf(asteroid_distance.universe, [100, 400, 700])
+        asteroid_distance['Medium'] = fuzz.trimf(asteroid_distance.universe, [350, 650, 950])
+        asteroid_distance['Far'] = fuzz.trimf(asteroid_distance.universe, [600, 900, 1200])
+        asteroid_distance['VFar'] = fuzz.smf(asteroid_distance.universe, 1000, 1300)
 
         # Asteroid size sets
-        asteroid_size['Small'] = fuzz.trimf(asteroid_size.universe, [10, 20, 40])
-        asteroid_size['Medium'] = fuzz.trimf(asteroid_size.universe, [30, 50, 70])
-        asteroid_size['Large'] = fuzz.smf(asteroid_size.universe, 60, 80)
+        asteroid_size['Small'] = fuzz.trimf(asteroid_size.universe, [1, 1, 2])
+        asteroid_size['Medium'] = fuzz.trimf(asteroid_size.universe, [1.5, 2.5, 3.5])
+        asteroid_size['Large'] = fuzz.trimf(asteroid_size.universe, [3, 4, 4])
 
         # Asteroid velocity sets
-        asteroid_velocity['Slow'] = fuzz.trimf(asteroid_velocity.universe, [0, 0, 150])
-        asteroid_velocity['Medium'] = fuzz.trimf(asteroid_velocity.universe, [100, 200, 300])
-        asteroid_velocity['Fast'] = fuzz.smf(asteroid_velocity.universe, 250, 350)
+        asteroid_velocity['Slow'] = fuzz.trimf(asteroid_velocity.universe, [0, 0, 100])
+        asteroid_velocity['Medium'] = fuzz.trimf(asteroid_velocity.universe, [50, 100, 150])
+        asteroid_velocity['Fast'] = fuzz.trimf(asteroid_velocity.universe, [100, 200, 200])
 
         # Output sets for turn rate
-        ship_turn['HardLeft'] = fuzz.trimf(ship_turn.universe, [-180, -180, -90])
-        ship_turn['Left'] = fuzz.trimf(ship_turn.universe, [-180, -90, 0])
-        ship_turn['Zero'] = fuzz.trimf(ship_turn.universe, [-45, 0, 45])
-        ship_turn['Right'] = fuzz.trimf(ship_turn.universe, [0, 90, 180])
-        ship_turn['HardRight'] = fuzz.trimf(ship_turn.universe, [90, 180, 180])
+        ship_turn['HardLeft'] = fuzz.trimf(ship_turn.universe, [-180,-180,-120])
+        ship_turn['Left'] = fuzz.trimf(ship_turn.universe, [-120,-60,60])
+        ship_turn['Zero'] = fuzz.trimf(ship_turn.universe, [-60,0,60])
+        ship_turn['Right'] = fuzz.trimf(ship_turn.universe, [-60,60,120])
+        ship_turn['HardRight'] = fuzz.trimf(ship_turn.universe, [120,180,180])
 
         # Output sets for thrust
         ship_thrust['Reverse'] = fuzz.trimf(ship_thrust.universe, [-500, -500, -400])
@@ -81,14 +92,14 @@ class FuzzyController(KesslerController):
         rules = []
 
         # Very close, large, fast asteroids
-        rules.append(ctrl.Rule(asteroid_distance['VClose'] & asteroid_size['Large'] & asteroid_velocity['Fast'], 
-                              (ship_turn['HardRight'], ship_thrust['Reverse'], ship_fire['No'], ship_mine['Yes'])))
+        # rules.append(ctrl.Rule(asteroid_distance["VClose"] & ~asteroid_size["Small"] & ~asteroid_velocity["Slow"] & (theta_delta["NL"] | theta_delta["NM"]), (ship_turn["Right"], ship_thrust["Forward"], ship_fire["No"], ship_mine["Yes"])))
+        # rules.append(ctrl.Rule(asteroid_distance["VClose"] & (theta_delta["PL"] | theta_delta["PM"]), (ship_turn["Left"], ship_thrust["Forward"], ship_fire["No"], ship_mine["Yes"])))
+        # rules.append(ctrl.Rule(asteroid_distance["VClose"] & (theta_delta["NS"] | theta_delta["Z"] | theta_delta["PS"]), (ship_turn["HardRight"], ship_thrust["Forward"], ship_fire["No"], ship_mine["Yes"])))
+        # rules.append(ctrl.Rule(asteroid_distance["VClose"] & (theta_delta["NS"] | theta_delta["Z"] | theta_delta["PS"]), (ship_turn["HardLeft"], ship_thrust["Forward"], ship_fire["No"], ship_mine["Yes"])))
 
-        rules.append(ctrl.Rule(asteroid_distance['VClose'] & asteroid_size['Medium'] & asteroid_velocity['Fast'], 
-                              (ship_turn['Right'], ship_thrust['Reverse'], ship_fire['No'], ship_mine['Yes'])))
-
-        rules.append(ctrl.Rule(asteroid_distance['VClose'] & asteroid_size['Large'] & asteroid_velocity['Medium'], 
-                              (ship_turn['Left'], ship_thrust['Reverse'], ship_fire['No'], ship_mine['Yes'])))
+        rules.append(ctrl.Rule(asteroid_distance['VClose'] & asteroid_size['Large']  & asteroid_velocity['Fast'],   (ship_turn['HardRight'], ship_thrust['Reverse'], ship_fire['Yes'], ship_mine['Yes'])))
+        rules.append(ctrl.Rule(asteroid_distance['VClose'] & asteroid_size['Medium'] & asteroid_velocity['Fast'],   (ship_turn['Right'], ship_thrust['Reverse'], ship_fire['No'], ship_mine['Yes'])))
+        rules.append(ctrl.Rule(asteroid_distance['VClose'] & asteroid_size['Large']  & asteroid_velocity['Medium'], (ship_turn['Left'], ship_thrust['Reverse'], ship_fire['No'], ship_mine['Yes'])))
 
         # Good shooting opportunities
         rules.append(ctrl.Rule(bullet_time['S'] & theta_delta['Z'],  (ship_turn['Zero'],  ship_thrust['Zero'], ship_fire['Yes'], ship_mine['No'])))
@@ -122,28 +133,25 @@ class FuzzyController(KesslerController):
             most_dangerous_asteroid: The predicted most dangerous asteroid for
                 the given ship based on the current state of the game.
         """
-        ship_pos = ship_state["position"]
-        ship_position = Vec2D(ship_pos[0], ship_pos[1])
+        ship_position = Vec2D(ship_state["position"])
         most_dangerous = None
         highest_threat = -1
 
         for asteroid in game_state["asteroids"]:
-            asteroid_pos = asteroid["position"]
-            asteroid_vel = asteroid["velocity"]
-            asteroid_position = Vec2D(asteroid_pos[0], asteroid_pos[1])
-            asteroid_velocity = Vec2D(asteroid_vel[0], asteroid_vel[1])
+            asteroid_position = Vec2D(asteroid["position"])
+            asteroid_velocity = Vec2D(asteroid["velocity"])
             ship_asteroid_vec = ship_position - asteroid_position
 
             # Calculate distance
             distance = ship_asteroid_vec.magnitude()
 
             # Threat calculation: closer, larger, faster = more dangerous
-            distance_threat = max(0, 1 - distance / 800.0)
-            size_threat = asteroid["size"] / 80
-            velocity_threat = min(1.0, asteroid_velocity.magnitude() / 350.0)
+            distance_threat = max(0, 1 - distance / 1300.0)
+            size_threat = asteroid["size"] / 40
+            velocity_threat = min(1.0, asteroid_velocity.magnitude() / 200.0)
 
-            # Combined threat score
-            threat_score = (0.5 * distance_threat + 0.3 * size_threat + 0.2 * velocity_threat)
+            # Combined threat score where we prioritize the distance.
+            threat_score = (0.6 * distance_threat + 0.3 * size_threat + 0.1 * velocity_threat)
 
             if threat_score > highest_threat:
                 highest_threat = threat_score
@@ -168,12 +176,9 @@ class FuzzyController(KesslerController):
                 asteroid. If the ship cannot target the given asteroid, then `bullet_time`
                 and `shooting_theta` will be `None`.
         """
-        ship_pos_dict = ship_state["position"]
-        asteroid_position_dict = asteroid["position"]
-        asteroid_velocity_dict = asteroid["velocity"]
-        ship_position     = Vec2D(ship_pos_dict[0], ship_pos_dict[1])
-        asteroid_position = Vec2D(asteroid_position_dict[0], asteroid_position_dict[1])
-        asteroid_velocity = Vec2D(asteroid_velocity_dict[0], asteroid_velocity_dict[1])
+        ship_position     = Vec2D(ship_state["position"])
+        asteroid_position = Vec2D(asteroid["position"])
+        asteroid_velocity = Vec2D(asteroid["velocity"])
 
         # Determine the vector that points from the asteroid to the ship.
         asteroid_ship_vec = ship_position - asteroid_position
@@ -235,14 +240,24 @@ class FuzzyController(KesslerController):
 
 
     def actions(self, ship_state: Dict, game_state: Dict) -> Tuple[float, float, bool, bool]:
-        """Main controller method called each time step"""
+        """Main controller method called each time step when the game updates its states.
+
+        Arguments:
+            ship_state: The state of the ship being updated.
+            game_state: The current state of the game.
+
+        Returns:
+            (`thrust`, `turn_rate`, `should_fire`, `should_drop_mine`): A tuple
+                containing the new thrust rate and new turn rate of the ship
+                along with two Boolean flags for if the ship should fire and if
+                it should drop a mine.
+        """
 
         # Find the most dangerous asteroid
         target_asteroid = self.find_most_dangerous_asteroid(ship_state, game_state)
 
         if target_asteroid is None:
-            # No asteroids
-            return 50.0, 45.0, False, False
+            return 50.0, 0.0, False, False
 
         # Calculate intercept parameters
         bullet_t, shooting_theta, distance, asteroid_vel = self.calculate_intercept(ship_state, target_asteroid)
@@ -250,7 +265,6 @@ class FuzzyController(KesslerController):
         # Create control system simulation
         controller = ctrl.ControlSystemSimulation(self.control_system, flush_after_run=1)
 
-        # Set inputs
         controller.input['bullet_time'] = min(bullet_t, 0.99) if bullet_t else 1.0
         controller.input['theta_delta'] = shooting_theta
         controller.input['asteroid_distance'] = min(distance, 999)
@@ -258,14 +272,12 @@ class FuzzyController(KesslerController):
         controller.input['asteroid_velocity'] = min(asteroid_vel, 399)
 
         try:
-            # Compute outputs
             controller.compute()
-
-            # Get defuzzified outputs
             thrust = float(controller.output['ship_thrust'])
             turn_rate = float(controller.output['ship_turn'])
             fire = bool(controller.output['ship_fire'] >= 0)
             drop_mine = bool(controller.output['ship_mine'] >= 0)
+
         except Exception as e:
             # Safe fallback behavior
             if distance < 200:  # Too close - evade
@@ -285,8 +297,6 @@ class FuzzyController(KesslerController):
                 drop_mine = bool(False)
 
         self.eval_frames += 1
-
-        # print(f"Thrust: {thrust}, Turn Rate: {turn_rate}, Fire: {fire}, Drop Mine: {drop_mine}")
         return thrust, turn_rate, fire, drop_mine
     
     @property
@@ -294,9 +304,30 @@ class FuzzyController(KesslerController):
         return "Fuzzy Controller"
 
 class Vec2D:
-    def __init__(self, x, y):
-        self._x = x
-        self._y = y
+    def __init__(self, a_tuple: Tuple[int | float, int | float]=None, x=None, y=None):
+        """Creates a vector from either a tuple or a set of x and y values.
+
+        Either the tuple must be specified or the x and y values. If both are
+        given, then a `ValueError` will be raised.
+
+        Arguments:
+            a_tuple: A tuple containing an x-value and a y-value representing
+                the x and y components of the vector.
+            x: The vector's x component value.
+            y: The vector's y component value.
+
+        Raises:
+            ValueError: If both `a_tuple` and `x` or `y` are specified.
+        """
+        if a_tuple and (x or y):
+            raise ValueError("Cannot provide both a tuple and coordinates to form a vector!")
+
+        if a_tuple:
+            self._x = a_tuple[0]
+            self._y = a_tuple[1]
+        else:
+            self._x = x
+            self._y = y
 
     @property
     def x(self):
@@ -320,13 +351,13 @@ class Vec2D:
         return math.atan2(self._y, self.x)
 
     def __mul__(self, other: Vec2D):
-        return Vec2D(self._x * other, self._y * other)
+        return Vec2D(x=self._x * other, y=self._y * other)
 
     def __rmul__(self, other):
-        return Vec2D(self._x * other, self._y * other)
+        return Vec2D(x=self._x * other, y=self._y * other)
 
     def __add__(self, other: Vec2D):
-        return Vec2D(self._x + other._x, self._y + other._y)
+        return Vec2D(x=self._x + other._x, y=self._y + other._y)
 
     def __sub__(self, other: Vec2D):
         """A new vector whose coordinates are the difference of the vectors.
@@ -336,7 +367,7 @@ class Vec2D:
         this vector is vector A and other is vector B, then the resulting
         vector will be the vector pointing from B to A.
         """
-        return Vec2D(self._x - other._x, self._y - other._y)
+        return Vec2D(x=self._x - other._x, y=self._y - other._y)
 
     def __str__(self):
         return f"<x={self._x:0.4f}, y={self._y:.4f}>"
